2854594947b25d5df03177d797fe05ab8f8130b5
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-15 09:25:34 -0700

    set state for subtitle, passed subtitle as property to the pres comp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,14 +8,18 @@ import './Home.css';
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('default state');
+  const [subtitle, setSubtitle] = useState('default state');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
       <Preview 
         title={title}
+        subtitle={subtitle}
+        font={font}
       />
       <Editor 
         title={title} setTitle={setTitle}
+        subtitle={subtitle} setSubtitle={setSubtitle}
       />
     </main>
   );
